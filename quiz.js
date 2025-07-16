8f9359d6afcdff98f31fd37c74e0f0d42332eb7d
Juliet Njagi <njagijuliet02@gmail.com>
2025-07-17 02:40:53 +0300

    made changes on my textcontent or method of display

--- quiz.js
+++ quiz.js
@@ -2,9 +2,9 @@ function checkAnswer() {
     // Function body
     let correctAnswer ="4";
     const selectedInput = document.querySelector(".quiz :checked");
-    let userAnswer= selectedInput.value ;
+    const userAnswer= selectedInput.value ;
     const feedback = document.getElementById("feedback");
-    if(userAnswer === correctAnswer){
+    if (userAnswer === correctAnswer){
         feedback.textContent ="correct! well done.";
     }else{
         feedback.textContent="That's incorrect. Try again!";
