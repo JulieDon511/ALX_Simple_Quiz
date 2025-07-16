e495858ea1d17bc3cf86eb2b8d60fd52fb4c3fcd
Juliet Njagi <njagijuliet02@gmail.com>
2025-07-17 02:24:05 +0300

    compared the user asnwers with the correct answer

--- quiz.js
+++ quiz.js
@@ -3,5 +3,10 @@ function checkAnswer() {
     let correctAnswer = 4;
     const corrAns = document.querySelector(".quiz");
     const userAnswer= corrAns.input ;
-    
+    if(userAnswer == correctAnswer){
+        corrAns.textContent ="correct! well done.";
+    }else{
+        corrAns.textContent="That's incorrect. Try again!";
+    }
+
 }
