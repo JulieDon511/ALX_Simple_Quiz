1d44e1ab5df0775f9cdd88805ed2c38e7280f97a
Juliet Njagi <njagijuliet02@gmail.com>
2025-07-17 08:51:54 +0300

    Changed my if statement so that it can check the checked or selected input

--- quiz.js
+++ quiz.js
@@ -1,15 +1,22 @@
 function checkAnswer() {
     // Function body
     let correctAnswer ="4";
-    const selectedInput = document.querySelector(".quiz :checked");
-    const userAnswer= selectedInput.value ;
-    // getting the feedback under button
-    const feedback = document.getElementById("feedback");
-    if (userAnswer === correctAnswer){
+    const selectedInput = document.querySelector('input[name="quiz"]:checked');
+
+    if(selectedInput){
+        const userAnswer= selectedInput.value ;
+        // getting the feedback under button
+        const feedback = document.getElementById("feedback");
+        if (userAnswer === correctAnswer){
         feedback.textContent ="Correct! Well done.";
     }else{
         feedback.textContent= "That's incorrect. Try again!";
     }
+    }else{
+        feedback.textContent = "Please select an answer.";
+        feedback.style.color = "orange";
+    }
+    
 }
 // button 
     const btn = document.getElementById("submit-answer");
