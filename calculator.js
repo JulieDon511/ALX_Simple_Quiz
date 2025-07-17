387114153f295d79bd34d2eb373bfe34df565193
Juliet Njagi <njagijuliet02@gmail.com>
2025-07-17 09:08:43 +0300

    Added new files of calculato

---
+++ calculator.js
@@ -0,0 +1,18 @@
+//addition
+function add(number1, number2) {
+    return number1 + number2;
+}
+//subtraction
+function subtract(number1, number2){
+    return number2 - number1;
+}
+
+// multiply
+function subtract(number1, number2){
+    return number1 *number2;
+}
+
+//divide
+function subtract(number1, number2){
+    return number2 / number1;
+}
