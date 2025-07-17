160b4e6edeaf4c0f861c3c0cfb9844d68e3704e2
Juliet Njagi <njagijuliet02@gmail.com>
2025-07-17 08:40:45 +0300

    Added the button on eventlistnera and parsed the function check answer

--- quiz.js
+++ quiz.js
@@ -3,11 +3,14 @@ function checkAnswer() {
     let correctAnswer ="4";
     const selectedInput = document.querySelector(".quiz :checked");
     const userAnswer= selectedInput.value ;
+    // getting the feedback under button
     const feedback = document.getElementById("feedback");
     if (userAnswer === correctAnswer){
         feedback.textContent ="Correct! Well done.";
     }else{
         feedback.textContent= "That's incorrect. Try again!";
     }
-
 }
+// button 
+    const btn = document.getElementById("submit-answer");
+    btn.addEventListener("click", checkAnswer);
