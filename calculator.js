2b1e196b72ab5643703dcc13bb2e1842f1255990
Juliet Njagi <njagijuliet02@gmail.com>
2025-07-17 09:56:54 +0300

    Added the button on event listner and it performs the calculation

--- calculator.js
+++ calculator.js
@@ -17,30 +17,30 @@ function divide(number1, number2){
     return number2 / number1;
 }
 //adding event listner
-document.getElementById(add).addEventListener("click", ()=>{
-    const no1 = parseFloat(number1).value || 0;
-    const no2 = parseFloat(number2).value || 0;
-    const results = add(no1, no2);
-    document.getElementById("calculation-result").textContent = results;
+document.getElementById('add').addEventListener('click', function() {
+    const number1 = parseFloat(document.getElementById('number1').value) || 0;
+    const number2 = parseFloat(document.getElementById('number2').value) || 0;
+    const result = add(number1, number2);
+    document.getElementById('calculation-result').textContent = result;
 });
 // subtract
-document.getElementById(subtract).addEventListener("click", ()=>{
-    const no1 = parseFloat(number1).value || 0;
-    const no2 = parseFloat(number2).value || 0;
+document.getElementById('subtract').addEventListener("click", ()=>{
+    const no1 = parseFloat(document.getElementById('number1').value) || 0;
+    const no2 = parseFloat(document.getElementById('number2').value) || 0;
     const results = subtract(no2, no1);
     document.getElementById("calculation-result").textContent = results;
 });
 //multiply
-document.getElementById(multiply).addEventListener("click", ()=>{
-    const no1 = parseFloat(number1).value || 0;
-    const no2 = parseFloat(number2).value || 0;
+document.getElementById('multiply').addEventListener("click", ()=>{
+   const no1 = parseFloat(document.getElementById('number1').value) || 0;
+    const no2 = parseFloat(document.getElementById('number2').value) || 0;
     const results = multiply(no1, no2);
     document.getElementById("calculation-result").textContent = results;
 });
 //divide
-document.getElementById(divide).addEventListener("click", ()=>{
-    const no1 = parseFloat(number1).value || 0;
-    const no2 = parseFloat(number2).value || 0;
+document.getElementById('divide').addEventListener("click", ()=>{
+    const no1 = parseFloat(document.getElementById('number1').value) || 0;
+    const no2 = parseFloat(document.getElementById('number2').value) || 0;
     const results = divide(no1, no2);
     document.getElementById("calculation-result").textContent = results;
 });
